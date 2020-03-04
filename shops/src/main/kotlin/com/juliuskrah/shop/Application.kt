046df66c243aff03120600c3b102876c09694d15
Julius Krah <juliuskrah@gmail.com>
2020-03-04 18:22:57 +0000

    modified to use the kotlin bean dsl

--- shops/src/main/kotlin/com/juliuskrah/shop/Application.kt
+++ shops/src/main/kotlin/com/juliuskrah/shop/Application.kt
@@ -3,7 +3,13 @@ package com.juliuskrah.shop
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.support.beans
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint
+import java.net.URI
 
 @EnableReactiveMethodSecurity
 @SpringBootApplication
@@ -12,5 +18,26 @@ class Application
 fun main(args: Array<String>) {
     runApplication<Application>(*args) {
         setBannerMode(Banner.Mode.OFF)
+        addInitializers(
+                beans {
+                    bean {
+                        val http = ref<ServerHttpSecurity>()
+                        val logoutHandler = OidcClientInitiatedServerLogoutSuccessHandler(ref())
+                        // todo logout uri per tenant
+                        logoutHandler.setPostLogoutRedirectUri(URI.create("http://theshop.com"))
+                        // todo auth entryPoint per tenant
+                        val authenticationEntryPoint = RedirectServerAuthenticationEntryPoint("/oauth2/authorization/shop")
+                        http.authorizeExchange {
+                            it.anyExchange().authenticated()
+                        }.logout {
+                            it.logoutSuccessHandler(logoutHandler)
+                        }.oauth2Login(Customizer.withDefaults())
+                                .exceptionHandling {
+                                    it.authenticationEntryPoint(authenticationEntryPoint)
+                                }
+                        http.build()
+                    }
+                }
+        )
     }
 }
--- shops/src/main/kotlin/com/juliuskrah/shop/ShopConfiguration.kt
+++ shops/src/main/kotlin/com/juliuskrah/shop/ShopConfiguration.kt
@@ -1,6 +1,5 @@
 package com.juliuskrah.shop
 
-
 import com.juliuskrah.shop.domain.Tenant
 import com.juliuskrah.shop.repository.TenantAwareRoutingConnectionFactory
 import com.juliuskrah.shop.security.TenantWebFilter
@@ -10,7 +9,6 @@ import io.r2dbc.spi.ConnectionFactory
 import io.r2dbc.spi.ConnectionFactoryOptions.*
 import io.r2dbc.spi.Option
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
@@ -18,15 +16,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.security.config.Customizer.withDefaults
-import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
-import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import java.net.URI
 import java.util.*
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -35,8 +26,6 @@ import java.util.regex.Pattern
 @EnableScheduling
 @EnableConfigurationProperties(ApplicationProperties::class)
 class ShopConfiguration : ApplicationContextAware {
-    @Autowired
-    private lateinit var clients: ReactiveClientRegistrationRepository
     private lateinit var applicationContext: ApplicationContext
     private val log = LoggerFactory.getLogger(ShopConfiguration::class.java)
 
@@ -98,24 +87,6 @@ class ShopConfiguration : ApplicationContextAware {
         matcher.appendTail(sb)
         println(sb.toString())
         return sb.toString()
-    }
-
-    @Bean
-    fun securityFilter(http: ServerHttpSecurity): SecurityWebFilterChain {
-        val logoutHandler = OidcClientInitiatedServerLogoutSuccessHandler(clients)
-        // todo logout uri per tenant
-        logoutHandler.setPostLogoutRedirectUri(URI.create("http://theshop.com"))
-        // todo auth entryPoint per tenant
-        val authenticationEntryPoint = RedirectServerAuthenticationEntryPoint("/oauth2/authorization/shop")
-        http.authorizeExchange {
-            it.anyExchange().authenticated()
-        }.logout {
-            it.logoutSuccessHandler(logoutHandler)
-        }.oauth2Login(withDefaults())
-                .exceptionHandling {
-                    it.authenticationEntryPoint(authenticationEntryPoint)
-                }
-        return http.build()
     }
 
     @Scheduled(initialDelay = 60000, fixedRate = 600000)
