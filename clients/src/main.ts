22de12861e61e3ba42a5583a56d283d2f61fb88f
juliuskrah <juliuskrah@gmail.com>
2020-11-11 15:25:43 +0000

    adding typescript support

--- clients/src/main.ts
+++ clients/src/main.ts
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import './assets/sass/shop.scss'
 
+declare global {
+    interface Window { Vue: any; }
+}
+
 Vue.config.productionTip = false
 Vue.component('top-menu', require('./components/TopMenu.vue').default);
+Vue.component('product-card', require('./components/ProductCard').default);
 window.Vue = Vue;
--- shops/src/main/kotlin/com/juliuskrah/shop/support/web/ShopUtilities.kt
+++ shops/src/main/kotlin/com/juliuskrah/shop/support/web/ShopUtilities.kt
@@ -2,5 +2,8 @@ package com.juliuskrah.shop.support.web
 
 object ShopUtilities {
     const val catalogViewName = "catalogs"
+    const val productViewName = "products"
+    const val checkoutViewName = "checkout"
+    const val cartViewName = "cart"
     const val indexViewName = "index"
 }
